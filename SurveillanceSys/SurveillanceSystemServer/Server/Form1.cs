a34ab092b7ef3701ea252e631cc371340bb5a3b2
quoctrungdh <quoctrungdh@gmail.com>
2016-05-16 14:50:51 +0700

    add smsnoti

--- SurveillanceSys/SurveillanceSystemServer/Server/Form1.cs
+++ SurveillanceSys/SurveillanceSystemServer/Server/Form1.cs
@@ -15,6 +15,7 @@ using System.Runtime.Serialization.Formatters.Binary;
 using System.IO;
 using System.Drawing.Imaging;
 
+
 namespace Server
 {
     public partial class Form1 : Form
@@ -109,7 +110,13 @@ namespace Server
                         {
                             Image recvImg = (Image)ByteArrayToObject(recvData);
                             pictureBox1.Image = recvImg;
-                            AddLbStatus("Received image from " + socket.RemoteEndPoint.ToString());
+
+                            string remoteEndPointInfo = socket.RemoteEndPoint.ToString();
+                            string alarmTime = DateTime.Now.ToShortDateString().ToString();
+                            string notiMess = "Camera " + remoteEndPointInfo + " phat hien chuyen dong vao luc " + alarmTime;
+                           // SendNoti("0905809054", notiMess);
+
+                            AddLbStatus("Received image from " + remoteEndPointInfo);
                             
                             string imgFileName = /*DateTime.Now.ToShortDateString().ToString()*/"abc.jpg";
                             Image saveImg = (Image)recvImg.Clone();
@@ -168,6 +175,77 @@ namespace Server
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
             Application.Exit();
+            
+        }
+
+        bool SendNoti(string phone, string message)
+        {
+            string url = "http://api.esms.vn/MainService.svc/xml/SendMultipleMessage_V2/";
+            // declare ascii encoding
+            UTF8Encoding encoding = new UTF8Encoding();
+
+            string strResult = string.Empty;
+
+
+            string customers = "";
+
+            string[] lstPhone = phone.Split(',');
+
+            for (int i = 0; i < lstPhone.Count(); i++)
+            {
+                customers = customers + @"<CUSTOMER>"
+                                + "<PHONE>" + lstPhone[i] + "</PHONE>"
+                                + "</CUSTOMER>";
+            }
+
+            //string SampleXml = @"<RQST>"
+
+            string APIKey = "F52DDAE968CBCF351AFEA429F38036";
+            string SecretKey = "0949AF1207C64D28B197A5D1A8AC41";
+
+            string SampleXml = @"<RQST>"
+                               + "<APIKEY>" + APIKey + "</APIKEY>"
+                               + "<SECRETKEY>" + SecretKey + "</SECRETKEY>"
+                               + "<ISFLASH>0</ISFLASH>"
+                               + "<SMSTYPE>7</SMSTYPE>"
+                               + "<CONTENT>" + message + "</CONTENT>"
+                               + "<CONTACTS>" + customers + "</CONTACTS>"
+
+
+           + "</RQST>";
+            string postData = SampleXml.Trim().ToString();
+            // convert xmlstring to byte using ascii encoding
+            byte[] data = encoding.GetBytes(postData);
+            // declare httpwebrequet wrt url defined above
+            HttpWebRequest webrequest = (HttpWebRequest)WebRequest.Create(url);
+            // set method as post
+            webrequest.Method = "POST";
+            webrequest.Timeout = 500000;
+            // set content type
+            webrequest.ContentType = "application/x-www-form-urlencoded";
+            // set content length
+            webrequest.ContentLength = data.Length;
+            // get stream data out of webrequest object
+            Stream newStream = webrequest.GetRequestStream();
+            newStream.Write(data, 0, data.Length);
+            newStream.Close();
+            // declare & read response from service
+            HttpWebResponse webresponse = (HttpWebResponse)webrequest.GetResponse();
+
+            // set utf8 encoding
+            Encoding enc = System.Text.Encoding.GetEncoding("utf-8");
+            // read response stream from response object
+            StreamReader loResponseStream =
+                new StreamReader(webresponse.GetResponseStream(), enc);
+            // read string from stream data
+            strResult = loResponseStream.ReadToEnd();
+            // close the stream object
+            loResponseStream.Close();
+            // close the response object
+            webresponse.Close();
+            // below steps remove unwanted data from response string
+            strResult = strResult.Replace("</string>", "");
+            return true;
         }
     }
 }
